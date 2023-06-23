909113907b5c23378311c9d00b396e3eb7f1d8f4
sofikry <krystofovasofie@gmail.com>
2023-06-23 15:01:19 +0200

    zacatek

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ing. V. Kryštof, znalec</title>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+      @import url("https://fonts.cdnfonts.com/css/metropolis-2?styles=21186,21180,21190,21183,21178,21179");
+    </style>
+  </head>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php' ?>
+  <body>
+    <div class="wrapper">
+      <header>
+        <div>
+          <h2>Ing. Vladimír Kryštof, znalec</h2>
+        </div>
+        <nav>
+          <div>&#9776;</div>
+          <ul>
+            <li><a href="index.html">Úvod</a></li>
+            <li>/</li>
+            <li><a href="posudky.html">Posudky</a></li>
+            <li>/</li>
+            <li><a href="kontakt.html">Kontakt</a></li>
+          </ul>
+        </nav>
+        <h1>Znalecké služby</h1>
+        <h3>pro obory ekonomika, kybernetika, spoje</h3>
+      </header>
+
+      <main>
+        <h2>Znalecké služby jsou určeny:</h2>
+        <p>Soukromým osobám a podnikům.</p>
+        <p>
+          Státním podnikům, rozpočtovým a příspěvkovým organizacím, státním
+          fondům, veřejnoprávním institucím.
+        </p>
+        <p>Policii ČR, advokátním kancelářím a soudům.</p>
+        <h2>Znalecké oprávnění:</h2>
+        <p>
+          Znalecká činnost je provozována na základě rozhodnutí předsedy
+          Krajského soudu v Brně
+        </p>
+        <p>č. j. Spr 1799/2004 a č. j. Spr 1404/2013-15 pro obory:</p>
+        <h3>Obor ekonomika, odvětví ceny a odhady</h3>
+        <p>
+          se specializací telekomunikace, sdělovací a počítačové sítě,
+          telemedicína, výpočetní technika.
+        </p>
+        <h3>Obor spoje, odvětví spoje</h3>
+        <p>
+          se specializací telekomunikace, telekomunikační zařízení, sdělovací a
+          přenosové sítě.
+        </p>
+        <h3>Obor kybernetika, odvětví výpočetní technika.</h3>
+      </main>
+      <footer>
+        <div>
+          Ing. Vladimír Kryštof, Bezručova čtvrť 839/15, 664 34 Kuřim,
+          <i class="fa fa-phone" aria-hidden="true"
+            ><a href="tel:+420 602 682 331"> +420 602 682 331</a>
+          </i>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
