26505263061b8f78112cde65019b47fa410d542b
sofikry <krystofovasofie@gmail.com>
2023-07-26 21:45:49 +0200

    responsivní mezi 650 a 800

--- posudky.php
+++ posudky.php
@@ -11,13 +11,16 @@
         <strong><a href="index.php" id="name">Ing. Vladimír Kryštof, znalec</a></strong>
 
         <nav>
+          <div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
 
-          <ul>
+          <ul class="menu">
+            <div class="close_menu"><i class="fa fa-times" aria-hidden="true"></i></div>
             <li><a href="index.php">úvod</a></li>
             <span>/</span>
             <li><a href="posudky.php">posudky</a></li>
             <span>/</span>
             <li><a href="kontakt.php">kontakt</a></li>
+
           </ul>
         </nav>
       </div>
